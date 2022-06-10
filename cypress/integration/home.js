d290905032f703e3bd26effb183ccb041e906a53
João Pedro Magalhães <joaopsilvamagalhaes@gmail.com>
2022-06-10 11:33:40 -0300

    Update home.js

--- cypress/integration/home.js
+++ cypress/integration/home.js
@@ -2,7 +2,7 @@ describe("Home", () => {
   it(`should render correctly`, () => {
     cy.visit(`${Cypress.env("baseUrl")}`);
 
-    cy.get("p").should("contain", "Edit src/App.js and save to reload.");
+    cy.get("p").should("contain", "Edit src/App.js and save to reloaddddd.");
     cy.get("a").should("contain", "Learn React");
     cy.get("p").should("contain", "Testando esse paragrafo");
   });
